1092c4538aa7cae0532fbab09328cc5840514c4a
eito-okada <eito8okada@gmail.com>
2021-10-23 22:52:58 +0900

    commit

--- Homepage/Progressbar.js/progressbar.js
+++ Homepage/Progressbar.js/progressbar.js
@@ -1,19 +1,9 @@
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+window.onload = function onLoad() {
+    var circle = new ProgressBar.Circle('#progress', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
 
-var bar = new ProgressBar.Line(container, {
-    strokeWidth: 4,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#FFEA82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'},
-    from: {color: '#FFEA82'},
-    to: {color: '#ED6A5A'},
-    step: (state, bar) => {
-        bar.path.setAttribute('stroke', state.color);
-    }
-});
-
-  bar.animate(1.0);  // Number from 0.0 to 1.0
+    circle.animate(1);
+};
